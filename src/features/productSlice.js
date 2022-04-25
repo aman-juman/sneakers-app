1e87591ff4d1f7a9b833bfad4df6c544e50dd22a
Aman <alamanshogun@gmail.com>
2022-04-25 09:32:39 +0600

    v2.3

--- src/features/productSlice.js
+++ src/features/productSlice.js
@@ -1,25 +1,28 @@
-// import {createSlice} from "@reduxjs/toolkit";
-//
-// const initialState = [
-//     {
-//         title: "Fall Limited Edition Sneakers",
-//         description: "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
-//         price: 250,
-//         currency: "USD",
-//         discount: 50,
-//     },
-// ];
-//
-// export const productSlice = createSlice({
-//     name: 'product',
-//     initialState,
-//     reducers: {
-//
-//     }
-// });
-//
-//
-// export const {} = productSlice.actions;
-// export default productSlice.reducer
-//
-//
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = [
+    {
+        title: "Fall Limited Edition Sneakers",
+        description: "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+        price: 250,
+        currency: "USD",
+        discount: 50,
+    },
+];
+
+export const productSlice = createSlice({
+    name: 'product',
+    initialState,
+    reducers: {
+        addProduct: (state, action) =>{
+
+        }
+
+    }
+});
+
+
+export const {addProduct} = productSlice.actions;
+export default productSlice.reducer
+
+
